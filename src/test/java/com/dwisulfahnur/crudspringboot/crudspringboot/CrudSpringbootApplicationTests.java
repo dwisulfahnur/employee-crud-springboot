ba244f91addecd01a40e87c99cbe4671bbff0823
Dwi Sulfahnur <dwisulfahnur@gmail.com>
2018-12-25 04:25:28 +0700

    Initial Commit

---
+++ src/test/java/com/dwisulfahnur/crudspringboot/crudspringboot/CrudSpringbootApplicationTests.java
@@ -0,0 +1,17 @@
+package com.dwisulfahnur.crudspringboot.crudspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CrudSpringbootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
